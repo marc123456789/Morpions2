60cb87b1d38a16b029790d7c339c9d6c8932b2ce
marc123456789 <marcgwada35@gmail.com>
2017-09-06 14:32:01 -0600

    Update robot.py

--- Robot/robot.py
+++ Robot/robot.py
@@ -1,10 +1,10 @@
 class Robot:
 
     def __init__(self, nom):
-        pass
+        self.nom = nom
 
     def __repr__(self):
         return "Je m'appelle {0} et je suis une intelligence artificiel !!".format(self.nom)
     
     def __str__(self):
-        self.__repr__()
+        return self.__repr__()
